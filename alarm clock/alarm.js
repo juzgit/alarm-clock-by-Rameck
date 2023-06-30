f485bf5f963fc064f2c8c837b09e96fc546751ce
Rameck-Junior Gadziso <rameckjuniorgit@gmail.com>
2023-06-30 16:50:32 +0200

    displayed date

--- alarm clock/alarm.js
+++ alarm clock/alarm.js
@@ -1,18 +1,30 @@
-function liveClock(){
-    const liveTime = new Date();
-    let hrs = liveTime.getHours();
-    let mins = liveTime.getMinutes();
-    let secs = liveTime.getSeconds();
-    mins = checkTime(mins);
-    secs = checkTime(secs);
-    document.getElementById('display-clock').textContent= hrs + ":" + mins + ":" + secs;
-    setTimeout(startTime, 1000);
+const liveClock = document.getElementById("display-clock");
+
+//show digital clock
+function liveTime(){
+    let digitalTime = new Date();
+    liveClock.textContent = digitalTime.toLocaleTimeString("en-ZA", 
+    {hour12:false});
+    setTimeout(liveTime,1000);
+}
+ liveTime();
+
+ // show digital date
+const currentDate = document.getElementById("date");
+
+function liveDate(){
+    let rightNow = new Date();
+    
+    let dayNum = rightNow.getDate();
+    let monthNum = rightNow.getMonth() + 1;
+    let yearNum = rightNow.getFullYear();
+
+
+    currentDate.textContent = dayNum + " / " + monthNum + " / " + yearNum;
 }
 
-function checkTime(i){
-    if (i <10){
-        i = "0" + i;
-    }
+liveDate();
 
-    return i;
-}
+
+
+
